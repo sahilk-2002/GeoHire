198a0df5c8f03976f7bd7441f1abdb379d04cdbf
Pranjal Joshi <joshi.pranjal5@gmail.com>
2026-06-04 19:00:46 +0530

    refactor: move home location setup from startup modal to Profile page
    - Remove intrusive startup modal from App.vue
    - Add 'My Home' section to Profile.vue with inline map (click to drop pin, save/remove)
    - Delete HomeLocationModal.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,32 +2,9 @@
   <div class="min-h-screen bg-background lg:pl-64">
     <router-view />
     <AppNav />
-    <HomeLocationModal v-if="showHomeModal" @confirm="onHomeConfirm" @skip="onHomeSkip" />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
 import AppNav from './components/AppNav.vue'
-import HomeLocationModal from './components/HomeLocationModal.vue'
-import { useResumeStore } from './stores/resume'
-
-const resumeStore = useResumeStore()
-const showHomeModal = ref(false)
-
-onMounted(() => {
-  if (resumeStore.homeLatitude == null && !localStorage.getItem('geohire_home_skipped')) {
-    showHomeModal.value = true
-  }
-})
-
-function onHomeConfirm(lat, lng) {
-  resumeStore.setHomeLocation(lat, lng)
-  showHomeModal.value = false
-}
-
-function onHomeSkip() {
-  localStorage.setItem('geohire_home_skipped', '1')
-  showHomeModal.value = false
-}
 </script>

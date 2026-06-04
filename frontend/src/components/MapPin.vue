15e09d8cd91c184a9ed3541a7f76ad1a2f0a53ac
sahilk-2002 <sahilkulkarni585@gmail.com>
2026-06-04 13:56:14 +0530

    feat: Leaflet map components (JobMap, MapPin)

---
+++ frontend/src/components/MapPin.vue
@@ -0,0 +1,14 @@
+<template>
+  <l-marker :lat-lng="[job.latitude || 0, job.longitude || 0]">
+    <l-tooltip>
+      <div class="text-sm font-semibold">{{ job.title }}</div>
+      <div class="text-xs">{{ job.company }} - {{ job.salary || 'Salary N/A' }}</div>
+    </l-tooltip>
+  </l-marker>
+</template>
+
+<script setup>
+import { LMarker, LTooltip } from '@vue-leaflet/vue-leaflet'
+
+defineProps({ job: Object })
+</script>
